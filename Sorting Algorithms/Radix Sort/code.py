765be1d56d51303fbc3d7b68ebf356a5bd9307f3
Shivaanjay Narula <115805065+ShivaanjayNarula@users.noreply.github.com>
2025-04-07 04:30:25 +0530

    Create code.py

---
+++ Sorting Algorithms/Radix Sort/code.py
@@ -0,0 +1,34 @@
+def counting_sort(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
+
+    for num in arr:
+        index = (num // exp) % 10
+        count[index] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    for i in reversed(range(n)):
+        index = (arr[i] // exp) % 10
+        output[count[index] - 1] = arr[i]
+        count[index] -= 1
+
+    for i in range(n):
+        arr[i] = output[i]
+
+def radix_sort(arr):
+    if not arr:
+        return arr
+
+    max_val = max(arr)
+    exp = 1
+    while max_val // exp > 0:
+        counting_sort(arr, exp)
+        exp *= 10
+
+    return arr
+
+arr = [170, 45, 75, 90, 802, 24, 2, 66]
+print(radix_sort(arr))
